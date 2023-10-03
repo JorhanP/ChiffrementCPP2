7788eba434e9b06ee27c8e6083e7711113ed754a
SNIR_admin <SNIR_admin@10-SNIR-14.560016n.ac-rennes.fr>
2023-10-03 15:23:04 +0200

    Ajout HashGestion.cpp et .h

--- chiffrementCpp.cpp
+++ chiffrementCpp.cpp
@@ -5,6 +5,7 @@
 #include "osrng.h"
 #include "hex.h"
 #include "RsaGestion.h"
+#include "HashGestion.h"
 
 #include <iostream>
 #include <string>
@@ -17,7 +18,7 @@ int main()
 
     RsaGestion NewKey;
 
-    cout << "//--------------------------------------Programme 1---------------------------------------------------//" << endl;
+    cout << "//--------------------------------------Programme 1 RSA---------------------------------------------------//" << endl;
 
     NewKey.generationClef("ClePublic.pem", "ClePrive.pem", 1024);
 
@@ -26,18 +27,24 @@ int main()
 
     cout <<NewKey.dechiffrementRsa(a)<<endl<<endl;
 
-    cout << "//--------------------------------------Programme 2---------------------------------------------------//" << endl;
+    cout << "//--------------------------------------Programme 2 RSA---------------------------------------------------//" << endl;
 
 
     NewKey.chargementClefs("ClePublic.pem", "ClePrive.pem");
     NewKey.chiffreDansFichier("Banane2", "FichierABanane.txt");
     cout << NewKey.dechiffreFichier("FichierABanane.txt") << endl;
 
-    cout << "//--------------------------------------Programme 5---------------------------------------------------//" << endl;
+    cout << "//--------------------------------------Programme 5 RSA---------------------------------------------------//" << endl;
+
 
     NewKey.chargementClefs("ClePublic.pem", "ClePrive.pem");
     NewKey.chiffrementFichier("Fichier1.txt", "Fichier2.txt");
     NewKey.dechiffrementFichier("Fichier2.txt", "Fichier1-1.txt");
+
+
+
+
+
 
 
     return 0;
